491f9797ad7bb35ec1ae600b80ee5d3500140e2d
Carlos Gonzalez C <dev.carlosgonzalezc@gmail.com>
2025-07-23 08:21:04 -0500

    Cambios en formulario.js antes de cambiar de rama

--- src/Componentes/formulario.js
+++ src/Componentes/formulario.js
@@ -3,7 +3,7 @@ import react from "react";
 function Formulario(){
     return (
         <div className="formulario-container">
-            console.log('Hola ');
+            
         </div>
     )
 }
