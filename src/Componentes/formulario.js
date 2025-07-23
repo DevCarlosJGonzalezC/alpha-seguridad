292c1e8317ad632cbd16891085936d4590c0e496
Carlos Gonzalez C <dev.carlosgonzalezc@gmail.com>
2025-07-23 08:31:25 -0500

    Configuracion del formulario

--- src/Componentes/formulario.js
+++ src/Componentes/formulario.js
@@ -3,7 +3,8 @@ import react from "react";
 function Formulario(){
     return (
         <div className="formulario-container">
-            
+            <div className="left"></div>
+            <div className="right"></div>
         </div>
     )
 }
